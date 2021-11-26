qiscus/qiscus-chat-sdk-android-sample

fix search contact

app/src/main/java/com/qiscus/mychatui/ui/adapter/ChatRoomAdapter.java
@@ -105,10 +105,18 @@ public class ChatRoomAdapter extends SortedRecyclerViewAdapter<QChatRoom, ChatRo
             name.setText(chatRoom.getName());
             QMessage lastComment = chatRoom.getLastMessage();
             if (lastComment != null && lastComment.getId() > 0) {
-                String lastMessageText = lastComment.isMyComment() ? "You: " : lastComment.getSender().getName().split(" ")[0] + ": ";
-                lastMessageText += chatRoom.getLastMessage().getType() == QMessage.Type.IMAGE
-                        ? "\uD83D\uDCF7 send an image" : lastComment.getMessage();
-                lastMessage.setText(lastMessageText);
+                if (lastComment.getSender() != null) {
+                    String lastMessageText = lastComment.isMyComment() ? "You: " : lastComment.getSender().getName().split(" ")[0] + ": ";
+                    lastMessageText += chatRoom.getLastMessage().getType() == QMessage.Type.IMAGE
+                            ? "\uD83D\uDCF7 send an image" : lastComment.getMessage();
+                    lastMessage.setText(lastMessageText);
+                }else{
+                    String lastMessageText = "";
+                    lastMessageText += chatRoom.getLastMessage().getType() == QMessage.Type.IMAGE
+                            ? "\uD83D\uDCF7 send an image" : lastComment.getMessage();
+                    lastMessage.setText(lastMessageText);
+                }
+
                 tv_time.setText(DateUtil.getLastMessageTimestamp(lastComment.getTimestamp()));
             } else {
                 lastMessage.setText("");

qiscus/qiscus-meet-android-sample

app/src/main/java/com/qiscus/mychatui/ui/adapter/ChatRoomAdapter.java
@@ -105,10 +105,18 @@ public class ChatRoomAdapter extends SortedRecyclerViewAdapter<QiscusChatRoom, C
             name.setText(chatRoom.getName());
             QiscusComment lastComment = chatRoom.getLastComment();
             if (lastComment != null && lastComment.getId() > 0) {
-                String lastMessageText = lastComment.isMyComment() ? "You: " : lastComment.getSender().split(" ")[0] + ": ";
-                lastMessageText += chatRoom.getLastComment().getType() == QiscusComment.Type.IMAGE
-                        ? "\uD83D\uDCF7 send an image" : lastComment.getMessage();
-                lastMessage.setText(lastMessageText);
+                if (lastComment.getSender() != null) {
+                    String lastMessageText = lastComment.isMyComment() ? "You: " : lastComment.getSender().split(" ")[0] + ": ";
+                    lastMessageText += chatRoom.getLastComment().getType() == QiscusComment.Type.IMAGE
+                            ? "\uD83D\uDCF7 send an image" : lastComment.getMessage();
+                    lastMessage.setText(lastMessageText);
+                }else{
+                    String lastMessageText = "";
+                    lastMessageText += chatRoom.getLastComment().getType() == QiscusComment.Type.IMAGE
+                            ? "\uD83D\uDCF7 send an image" : lastComment.getMessage();
+                    lastMessage.setText(lastMessageText);
+                }
+
                 tv_time.setText(DateUtil.getLastMessageTimestamp(lastComment.getTime()));
             } else {
                 lastMessage.setText("");