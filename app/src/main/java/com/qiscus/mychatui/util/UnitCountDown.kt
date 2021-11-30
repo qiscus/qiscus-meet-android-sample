4271a3c0668513ad5bdc19447dd1c60aa5e9a996
pengdst <pengkuh.dst@gmail.com>
2021-11-30 15:29:51 +0700

    create UnitCountDown.kt

---
+++ app/src/main/java/com/qiscus/mychatui/util/UnitCountDown.kt
@@ -0,0 +1,28 @@
+package com.qiscus.mychatui.util
+
+import android.os.CountDownTimer
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created on 6/14/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+abstract class UnitCountDown(timer: Long, unit: TimeUnit, interval: Long = 1) : CountDownTimer(
+    TimeUnit.MILLISECONDS.convert(timer, unit),
+    TimeUnit.MILLISECONDS.convert(interval, unit)
+) {
+
+    override fun onTick(millisUntilFinished: Long) {
+        onUnitTick(TimeUnit.SECONDS.convert(millisUntilFinished, TimeUnit.MILLISECONDS))
+    }
+
+    override fun onFinish() {
+        this.cancel()
+    }
+
+    abstract fun onUnitTick(secondsUntilFinished: Long, unit: TimeUnit = TimeUnit.SECONDS)
+
+}
